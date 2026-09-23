438bfc2d18dcfaf5afb26ea258249f2d6cea2ccb
AndrewBartsch42 <123200636+AndrewBartsch42@users.noreply.github.com>
2026-09-23 13:27:18 -0600

    added comment for test passing currently in cryptomathTests

--- cryptomath/cryptomathTests.py
+++ cryptomath/cryptomathTests.py
@@ -5,6 +5,7 @@ from gcd import findGCD
 #test cases for base gcd function
 #test case 1: tests for two positive numbers
 #test case 2: tests for two negative numbers
+#all test cases for findGCD pass
 class gcdTest(unittest.TestCase):
     def test_gcd_pos(self):
         self.assertEqual(findGCD(4883,4369), 257)
