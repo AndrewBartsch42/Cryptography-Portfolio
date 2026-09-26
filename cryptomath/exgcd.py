368936a9144d189a0e7bc021b30c217218bbb62d
Andrew Bartsch <123200636+AndrewBartsch42@users.noreply.github.com>
2026-09-25 21:08:42 -0600

    finished extended gcd function needs tests to be written

--- cryptomath/exgcd.py
+++ cryptomath/exgcd.py
@@ -1,22 +1,16 @@
 def findExGCD(a: int, b:int):
-    s = 0
-    r = b
-    old_s = 1
-    old_r = a
-
-    while r != 0:
-        quotient = old_r/r
-        (old_r, r) = (r, old_r - quotient * r)
-        (old_s, s) = (s, old_s - quotient * s)
-
-    if b != 0:
-        bezout_t = (old_r - old_s * a)/b
-    else:
-        bezout_t = 0
-
-    output = {"coefficent a": old_s, "coefficent b":bezout_t, "gcd":old_r}
-    return output
+    if a == 0:
+        return b, 0, 1
+    gcd, x1, y1 = findExGCD(b % a, a)
+    x = y1 - (b // a) * x1
+    y = x1
+    return gcd, x, y
 
 
-print(findExGCD(5,11))
+
+
+a, b = 35, 15
+g, x, y = findExGCD(a, b)
+print("GCD is ", g)
+print("x= ", x, ", y= ", y)
     
